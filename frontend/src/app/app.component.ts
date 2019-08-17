cd1f665e9ca43c7c99e0aec987d9d0c090280cc3
er-chaan <er.chandreshbhai@gmail.com>
2019-08-17 12:15:19 +0530

    header footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  // title = 'frontend';
-  bodyClasses = 'skin-blue sidebar-mini';
-  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
-  title = 'angularstart7';
-  ngOnInit() {
-    this.body.classList.add('skin-blue');
-    this.body.classList.add('sidebar-mini');
-  }
+  title = 'frontend';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
