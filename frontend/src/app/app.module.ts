065d440de15656bf4419bfb795162128988740f8
er-chaan <er.chandreshbhai@gmail.com>
2019-08-18 18:41:03 +0530

    base structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { NavigationComponent } from './components/layout/navigation/navigation.component';
+import { ContentComponent } from './components/layout/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavigationComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
